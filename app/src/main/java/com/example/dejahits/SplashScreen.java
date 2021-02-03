3828edcdd461e41be14494de8760cd514d80abe6
Pirates <ican.notfound@gmail.com>
2021-02-03 15:16:58 +0800

    splash screen add!

---
+++ app/src/main/java/com/example/dejahits/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.dejahits;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    private int waktu= 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, waktu);
+    }
+}
