cee407ab32457b6da266b3ac9b8c2ef765d71b34
Pirates <ican.notfound@gmail.com>
2021-02-03 13:57:06 +0800

    meta dejahits

---
+++ app/src/main/java/com/example/dejahits/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.dejahits;
+
+import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton arrow;
+    LinearLayout hiddenView;
+    CardView cardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cardView = findViewById(R.id.base_cardview);
+        arrow = findViewById(R.id.arrow_button);
+        hiddenView = findViewById(R.id.hidden_view);
+
+        arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (hiddenView.getVisibility() == View.VISIBLE) {
+                    TransitionManager.beginDelayedTransition(cardView,
+                            new AutoTransition());
+                    hiddenView.setVisibility((View.GONE));
+                    arrow.setImageResource(R.drawable.ic_baseline_expand_more_24);
+                }
+
+                else {
+                    TransitionManager.beginDelayedTransition(cardView,
+                            new AutoTransition());
+                    hiddenView.setVisibility(View.VISIBLE);
+                    arrow.setImageResource(R.drawable.ic_baseline_expand_less_24);
+                }
+            }
+        });
+    }
+}
